ddfc38e109f40bcf599826161ad6c8f436d6eac8
nikothomas <nikojb@gmail.com>
2024-12-04 04:54:08 -0800

    removed unnecessary managers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,47 +1,14 @@
 mod handle_ctd_data;
 mod handle_nutrient_ammonia;
 mod handle_sequence_data;
-mod db_manager;
-mod kraken_uniq_manager;
 
 use tauri::Manager;
 use handle_ctd_data::handle_ctd_data_upload;
 use handle_nutrient_ammonia::handle_nutrient_ammonia;
 use handle_sequence_data::handle_sequence_data;
-use db_manager::DbManager;
-use kraken_uniq_manager::KrakenManager;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            // Clone necessary handles to avoid moving the non-Send `app` into the async block
-            let app_handle = app.handle().clone(); // Get a cloneable handle to the app
-
-            // Perform initialization in a separate async task
-            tauri::async_runtime::spawn(async move {
-                // Initialize your app
-                println!("Initializing...");
-                match DbManager::ensure_database(&app_handle).await {
-                    Ok(db_path) => println!("Database ready at: {:?}", db_path),
-                    Err(e) => eprintln!("Database setup failed: {}", e),
-                }
-                println!("Done initializing.");
-                println!("Initializing Kraken...");
-                match KrakenManager::ensure_krakenuniq(&app_handle).await {
-                    Ok(db_path) => println!("Database ready at: {:?}", db_path),
-                    Err(e) => eprintln!("Database setup failed: {}", e),
-                }
-                println!("Done initializing Kraken.");
-                // After initialization, close the splashscreen and show the main window
-                let app_handle_clone = app_handle.clone();
-                tauri::async_runtime::spawn_blocking(move || {
-                    let _ = app_handle_clone.get_window("splashscreen").unwrap().close();
-                    let _ = app_handle_clone.get_window("main").unwrap().show();
-                });
-            });
-
-            Ok(())
-        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
