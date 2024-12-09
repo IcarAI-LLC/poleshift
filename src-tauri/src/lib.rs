ebe918f6178bd9dc309724548f1a5b2a0b560d0f
nikothomas <nikojb@gmail.com>
2024-12-09 14:20:35 -0800

    backend fixes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,7 +15,6 @@ use process_sidebar_stats::process_sidebar_stats;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_cli::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
